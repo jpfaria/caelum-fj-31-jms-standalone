27ba54812c5954482e2e91b51750c530f855265a
João Paulo Faria <jpfaria@gmail.com>
2013-06-28 00:04:34 -0300

    .

---
+++ src/br/com/caelum/jms/EnviaMensagemParaFila.java
@@ -0,0 +1,48 @@
+package br.com.caelum.jms;
+
+import java.util.Properties;
+import java.util.Scanner;
+
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueReceiver;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+
+public class EnviaMensagemParaFila {
+
+	public static void main(String[] args) throws NamingException, JMSException {
+		
+		Properties jndiProperties = new Properties();
+		jndiProperties.put(Context.SECURITY_PRINCIPAL, "jms");
+		jndiProperties.put(Context.SECURITY_CREDENTIALS, "caelum");
+		
+		InitialContext ic = new InitialContext(jndiProperties);
+		
+		QueueConnectionFactory qcf = (QueueConnectionFactory) ic.lookup("jms/RemoteConnectionFactory");
+		
+		QueueConnection qc = qcf.createQueueConnection("jms", "caelum");
+		
+		QueueSession qs = qc.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		TextMessage tm = qs.createTextMessage();
+		tm.setText("Mensagem de texto para um Queue");
+		
+		Queue q = (Queue) ic.lookup("jms/queue/loja");
+		
+		QueueSender sender = qs.createSender(q);
+		sender.send(tm);
+		
+		qc.close();
+
+	}
+
+}
